28f8fc2b9f359c8a99a36018a8e38380e6d90b1e
wan <47422904+safwanAzman@users.noreply.github.com>
2024-08-01 21:29:47 +0800

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <footer className="fixed bottom-0 w-full p-2 bg-black ">
           <div className="flex items-center justify-center text-xs text-white">
             Challenge by <a className="px-1 text-blue-300 border-b-2 border-blue-300" href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-            Coded by <a className="pl-1 text-blue-300 border-b-2 border-blue-300" href="https://portfolio-v2-self.vercel.app/"> SafwanAzman.</a>
+            Coded by <a className="pl-1 text-blue-300 border-b-2 border-blue-300" href="https://www.safwan-azman.dev"> SafwanAzman.</a>
           </div>
         </footer>
     </div>
